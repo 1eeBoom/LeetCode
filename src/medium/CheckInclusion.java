2c91068c705960756b8a8082a6983b97871874a4
lipeng <54coolas>
2019-01-15 22:55:02 +0800

    2019年01月15日

--- src/medium/CheckInclusion.java
+++ src/medium/CheckInclusion.java
@@ -1,4 +1,27 @@
 package medium;
 
+import java.util.Arrays;
+
 public class CheckInclusion {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] a = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            a[s1.charAt(i)-'a']++;
+        }
+        int count = s2.length()-s1.length()+1;
+
+        int len = s1.length();
+        //起始位置
+        for (int i = 0; i < count; i++) {
+            int[] b = new int[26];
+            //滑动窗口
+            for (int j = i; j < i+len ; j++) {
+                b[s2.charAt(j)-'a']++;
+            }
+            if(Arrays.equals(a,b)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
