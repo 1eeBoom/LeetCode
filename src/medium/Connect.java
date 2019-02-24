6873c106ed2623e6c69e96a79967968d8f0061ec
1eeBoom <1922258827@qq.com>
2019-02-24 16:28:53 +0800

    2019年2月24日16:28:59 填充每个节点的下一个右侧节点指针

---
+++ src/medium/Connect.java
@@ -0,0 +1,66 @@
+package medium;
+
+/**
+ * Created By LeeBoom On 2019/2/24 15:27
+ */
+
+import struct.TreeLinkNode;
+
+/**
+ * 填充每个节点的下一个右侧节点指针
+ * 给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+ *
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ *
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ *
+ *      1 -> NULL
+ *    /  \
+ *   2 -> 3 -> NULL
+ *  / \  / \
+ * 4->5->6->7 -> NULL
+ *
+ * 输入：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":{"$id":"6","left":null,"next":null,"right":null,"val":6},"next":null,"right":{"$id":"7","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+ *
+ * 输出：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":{"$id":"6","left":null,"next":null,"right":null,"val":7},"right":null,"val":6},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"7","left":{"$ref":"5"},"next":null,"right":{"$ref":"6"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"7"},"val":1}
+ *
+ * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+ *
+ *
+ * 提示：
+ *
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ */
+
+public class Connect {
+    public void connect(TreeLinkNode root) {
+        //递归终止条件:当前节点为空
+        if(root == null)
+        {
+            return ;
+        }
+
+        //因为是完美二叉树,如果有左子树,一定有右子树.
+        if(root.left != null)
+        {
+            //将节点的左子树和右子树相连
+            root.left.next = root.right;
+            // 如果节点next不为空,那么将节点右子树指向节点next的左子树
+            if(root.next != null)
+            {
+                root.right.next = root.next.left;
+            }
+        }
+
+        //对左子树和右子树递归
+        connect(root.left);
+        connect(root.right);
+    }
+}
---
+++ src/struct/TreeLinkNode.java
@@ -0,0 +1,10 @@
+package struct;
+
+/**
+ * Created By LeeBoom On 2019/2/24 15:29
+ */
+public class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left, right, next;
+    public TreeLinkNode(int x) { val = x; }
+}
