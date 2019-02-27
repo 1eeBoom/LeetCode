f327a11f4a24529f9011db615c9f2860cf6a04bc
1eeBoom <1922258827@qq.com>
2019-02-27 23:44:33 +0800

    2019年2月27日23:44:21 电话号码的字母组合

--- src/medium/LetterCombinations.java
+++ src/medium/LetterCombinations.java
@@ -4,7 +4,7 @@ package medium;
  * Created By LeeBoom On 2019/2/20 23:58
  */
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 电话号码的字母组合
@@ -22,6 +22,38 @@ import java.util.List;
  * 尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
  */
 public class LetterCombinations {
-
+    public List<String> letterCombinations(String digits) {
+        char[] nums = digits.toCharArray();
+        List<String> list = new LinkedList<>();
+        if("".equals(digits))
+        {
+            return list;
+        }
+        list.add("");
+        Map<Character,String> map = new HashMap<>();
+        map.put('0',"");
+        map.put('1',"");
+        map.put('2',"abc");
+        map.put('3',"def");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
+        //第一层循环，对输入的数字数组进行循环，分别取出每个数字，以及对应的映射字符串
+        //第二层循环，对队列中的元素进行循环，分别取出待用
+        //第三层循环，对映射字符串的元素进行循环，分别将其加入到队列中的元素构成新的字符串并重新加入到队列中
+        for (char i : nums) {
+            int size = list.size();
+            for (int j = 0; j < size; j++) {
+                String s = ((LinkedList<String>) list).remove();
+                for (char c : map.get(i).toCharArray()) {
+                    list.add(s+c);
+                }
+            }
+        }
+        return list;
+    }
 
 }
